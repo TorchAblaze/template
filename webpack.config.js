18501421bbf4ddb7eeb1814fa3e8af51e9672761
TorchAblaze <tiffanygreathead@gmail.com>
2021-04-15 20:09:31 -0700

    Add webpack and plugin/loader files

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const HtmlWebpackPlugin = require(`html-webpack-plugin`)
+const { CleanWebpackPlugin } = require(`clean-webpack-plugin`)
+
+module.exports = {
+  entry: `./src/js/app.js`,
+  output: {
+    path: `${__dirname}/dist`,
+    filename: `bundle.js`,
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: `TITLE`,
+      template: `src/index.html`, // template file
+      filename: `index.html`, // output file
+      inject: true,
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/,
+        use: [`style-loader`, `css-loader`, `sass-loader`],
+      },
+      {
+        test: /\.(svg|gif|png|eot|woff(2)?|ttf)$/,
+        use: [`url-loader`],
+      },
+    ],
+  },
+  // dev-specific content
+  mode: `development`,
+  devtool: `source-map`,
+  devServer: { contentBase: `./dist` },
+}
